d50bf0ff6be00c87387d1489c604b865c883db95
Narfyx <kevin.abribat@live.fr>
2024-06-30 06:17:26 +0200

    correction bug user DELETE

--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics, permissions, status, viewsets
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import User
@@ -23,10 +26,44 @@ class MyTokenObtainPairView(TokenObtainPairView):
     permission_classes = [IsOwner]
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsOwner]
+class UserDetail(APIView):
+    permission_classes = [IsAuthenticated]  # Default for all methods
+
+    def get_permissions(self):
+        # Set specific permissions for each method
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            self.permission_classes = [IsOwner]
+        elif self.request.method == "GET":
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+    def get(self, request):
+        user = self.get_object()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = self.get_object()
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request):
+        user = self.get_object()
+        serializer = UserSerializer(
+            user, data=request.data, partial=True
+        )  # Utilisation de `partial=True` pour les mises à jour partielles
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        user = self.get_object()
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_object(self):
         return self.request.user
